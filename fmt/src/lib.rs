d2d350f8529d5604317d4b740d64873b5524b826
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2024-03-05 17:02:23 -0500

    better func names

--- fmt/src/lib.rs
+++ fmt/src/lib.rs
@@ -4,6 +4,6 @@ extern crate alloc;
 use alloc::string::String;
 use core::fmt::Write as _;
 
-pub fn my_test_func(buffer: &mut String, value: i32) -> core::fmt::Result {
+pub fn my_test_func_with_param(buffer: &mut String, value: i32) -> core::fmt::Result {
     write!(buffer, "Iteration {value} was written")
 }
--- str/src/lib.rs
+++ str/src/lib.rs
@@ -4,6 +4,6 @@ extern crate alloc;
 use alloc::string::String;
 use core::fmt::Write as _;
 
-pub fn my_test_func(buffer: &mut String) -> core::fmt::Result {
+pub fn my_test_func_with_simple_str(buffer: &mut String) -> core::fmt::Result {
     write!(buffer, "Simple inline string")
 }
